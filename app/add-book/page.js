4fd5997b41d64e5c89c1708339f009748d4ea489
Muhammad Hassan Saggaf <muhammadsaggaf2004@gmail.com>
2025-07-23 17:36:10 +0300

    feat: create page component for adding books with Navbar and AddBooks integration

---
+++ app/add-book/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddBooks from '../src/components/AddBooks'
+import Navbar from '../src/components/NavBar'
+
+const page = () => {
+  return (
+    <div><Navbar/>
+      <AddBooks /></div>
+  )
+}
+
+export default page
